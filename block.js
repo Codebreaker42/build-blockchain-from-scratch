a6181e83d21ee87c1d220b03197d959af76de5a3
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-04-13 18:17:36 +0530

    how genesis block and block minning logic is  done

--- block.js
+++ block.js
@@ -1,29 +1,40 @@
-
-
-
+// importing the genesis block 
+const {GENESIS_DATA} = require('./config');
+const hashValue = require('./crypto_hash');
+// importing the hash value
+const hash=require('./crypto_hash');
 class Block{
-    constructor({_timestamp,_prevHash,_hash,_data}){
-        this.timestamp=_timestamp;
-        this.prevHash=_prevHash;
-        this.hash=_hash;
-        this.data=_data;
+    constructor(_timestamp, _prevHash, _hash, _data){
+        this.timestamp = _timestamp;
+        this.prevHash = _prevHash;
+        this.hash = _hash;
+        this.data = _data;
+    }
+    
+    static getGenesisBlock(){
+        return new this(GENESIS_DATA.timestamp, GENESIS_DATA.prevHash, GENESIS_DATA.hash, GENESIS_DATA.data);
+    }
+
+    //minning the block
+    static mineBlock({prevBlock,data}){
+        let timestamp=Date.now() // return the current timestamp
+        let prevHash=prevBlock.hash;
+        let hash=hashValue(timestamp,prevHash,data);
+        return new this(
+          timestamp,
+          prevHash,
+          hash,
+          data,
+      );
     }
 }
 
 // creating object
-const block1=new Block({
-  _timestamp: "12/09/2024",
-  _prevHash:"0x123",
-  _hash:"0x456",
-  _data:"block"
-});
-
-const block2=new Block({ // order doesn't matter because we passing the objects 
-  _timestamp:"13/09/2024",
-  _hash:"0x789",
-  _prevHash:"0x456",
-  _data:"chain"
-});
-
+const block1 = new Block("12/09/2024", "0x123", "0x456", "block");
 console.log(block1);
-console.log(block2);
+
+const genesisBlock = Block.getGenesisBlock();
+console.log(genesisBlock);
+
+const result=Block.mineBlock({prevBlock:block1,data:'block2'});
+console.log(result);
