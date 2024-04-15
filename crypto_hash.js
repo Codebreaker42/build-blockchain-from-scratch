a9d127ae02ed75e26e634581f821dd3dd6a2c63b
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-04-15 13:22:52 +0530

    update hash function

--- crypto_hash.js
+++ crypto_hash.js
@@ -1,11 +1,10 @@
 // importing the crypto module
-
 const crypto=require('crypto');
-
+// const hexToBinary=require('hex-to-binary'); // importing the predefine library to convert hex code into the binary numbers
 const hashValue= (...inputs)=>{ //used to take the argument as many as we want
     hash=crypto.createHash('sha256');
     hash.update(inputs.sort().join(""))// concatinate all the parameter of the function
-    return hash.digest("hex") // returning the hex hash value
+    return hash.digest("hex"); 
 }
 module.exports=hashValue; // exporting the hash value
 //console.log(hashValue("hello","world"));
