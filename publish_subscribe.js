46cfb8b1c64f5839f4f04634dd8bb1b006c550d1
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-04-15 22:02:30 +0530

    adding the functionality of broadcasting the block to each node

--- publish_subscribe.js
+++ publish_subscribe.js
@@ -11,24 +11,33 @@ const channels={
 
 class PublishSubscribe{
     constructor({blockchain}){
-        this.blockchain=blockchain // accepting the blockchain instance
+        this.blockchain=blockchain; // accepting the blockchain instance
+        // console.log(this.blockchain);
         this.publisher=redis.createClient(); //defining the publisher
         this.subscriber=redis.createClient(); //defining the subscriber
         this.subscriber.subscribe(channels.TEST); //subscribe the channel by the subscriber
         this.subscriber.subscribe(channels.BLOCKCHAIN); // subscribe the blockchain channel by the subscriber
-        this.subscriber.on('Message',(channel,message)=>this.handleMessage(channel,message));
+        this.subscriber.on('message',(channel,message)=>
+        this.handleMessage(channel,message)
+      );
     }
     handleMessage(channel,message){
+        // console.log('hello');
         console.log(`Message recieved.Channel:${channel}, Message:${message}`);
         const parseMessage=JSON.parse(message);
+        // console.log(parseMessage);
         if(channel===channels.BLOCKCHAIN){
+            console.log('k');
             this.blockchain.replaceChain(parseMessage);
         }
     }
     publish({channel,message}){
+        // console.log(channel);
+        // console.log(message);
         this.publisher.publish(channel,message);
     }
     broadcastChain(){
+        // console.log(this.blockchain);
         this.publish({
             channel: channels.BLOCKCHAIN,
             message: JSON.stringify(this.blockchain.chain),
@@ -36,7 +45,7 @@ class PublishSubscribe{
     }
 }
 /* for testing purpose:
-// const checkPubSub= new PublishSubscribe();
-// setTimeout(()=>checkPubSub.publisher.publish(channels.TEST,"Hello JS"),1000); 
+const checkPubSub= new PublishSubscribe(({blockchain:'b1'}));
+setTimeout(()=>checkPubSub.publisher.publish(channels.TEST,"Hello JS"),1000); 
 */
 module.exports=PublishSubscribe;
